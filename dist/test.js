49bd0fc8dd8aa2727396cb1f8f575968f8649ba6
CooLNuanfeng <1634898698@qq.com>
2016-09-22 15:03:11 +0800

    demo

--- dist/test.js
+++ dist/test.js
@@ -1,28 +1,150 @@
 // 运行命令  babel -w --presets react src --out-dir dist
-var LikeButton = React.createClass({
-    displayName: "LikeButton",
 
-    getInitialState: function () {
-        return {
-            like: false
-        };
-    },
-    handleClick: function () {
-        this.setState({
-            like: !this.state.like
-        });
-    },
+// one
+var HelloMessage = React.createClass({
+    displayName: 'HelloMessage',
+
     render: function () {
-        var text = this.state.like ? 'Like' : "don't like";
         return React.createElement(
-            "p",
-            { onClick: this.handleClick },
-            this.props.name,
-            " hello welcome,You ",
-            text,
-            " this. Click toggle."
+            'h1',
+            null,
+            'Hello world, hello React!'
         );
     }
 });
 
-ReactDOM.render(React.createElement(LikeButton, { name: "blue" }), document.getElementById('example'));
+//two
+var Parent = React.createClass({
+    displayName: 'Parent',
+
+    getDefaultProps: function () {
+        //设置 props 默认值
+        return {
+            context: 'default'
+        };
+    },
+    propTypes: { //设置 props 的 类型和是否必须
+        context: React.PropTypes.string.isRequired
+    },
+    getInitialState: function () {
+        return {
+            context: this.props.context
+        };
+    },
+    render: function () {
+        console.log('Parent');
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h1',
+                null,
+                'This is Parent Title'
+            ),
+            React.createElement(Child, { content: this.state.context })
+        );
+    }
+});
+
+var Child = React.createClass({
+    displayName: 'Child',
+
+    getInitialState: function () {
+        return {
+            content: this.props.content
+        };
+    },
+    render: function () {
+        console.log('Child');
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h2',
+                null,
+                'This is a Child Title'
+            ),
+            React.createElement(
+                'p',
+                null,
+                this.state.content
+            ),
+            React.createElement(
+                'button',
+                { onClick: this.change },
+                'Change'
+            )
+        );
+    },
+    change: function () {
+        this.setState({
+            content: 'OK'
+        });
+    }
+});
+
+//three
+var NotesList = React.createClass({
+    displayName: 'NotesList',
+
+    render: function () {
+        return React.createElement(
+            'ol',
+            null,
+            React.Children.map(this.props.children, function (child, index) {
+                console.log(child, index);
+                return React.createElement(
+                    'li',
+                    null,
+                    child
+                );
+            })
+        );
+    }
+});
+
+//four
+
+var InputText = React.createClass({
+    displayName: 'InputText',
+
+    getInitialState: function () {
+        return {
+            value: 'aa'
+        };
+    },
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement('input', { type: 'text', ref: 'myInput', onChange: this.change }),
+            React.createElement(
+                'button',
+                { onClick: this.focus },
+                'focus'
+            ),
+            React.createElement(
+                'p',
+                null,
+                this.state.value
+            )
+        );
+    },
+    change: function (ev) {
+        this.setState({
+            value: this.refs.myInput.value
+        });
+        // or
+        // this.setState({
+        //     value : $(ev.target).val()
+        // });
+    },
+    focus: function (ev) {
+        console.log(ev.target);
+        // 可以通过 event 对象 或 ref 来获取 DOM
+        $(ev.target).prev().val('aa');
+        $(this.refs.myInput).focus();
+    }
+});
+
+ReactDOM.render(React.createElement(InputText, null), document.getElementById('test'));
--- src/test.js
+++ src/test.js
@@ -1,24 +1,116 @@
 // 运行命令  babel -w --presets react src --out-dir dist
-var LikeButton = React.createClass({
-    getInitialState : function(){
-        return {
-            like : false
-        }
-    },
-    handleClick : function(){
-        this.setState({
-            like : !this.state.like
-        });
-    },
+
+// one
+var HelloMessage = React.createClass({
     render : function(){
-        var text = this.state.like ? 'Like' : "don't like";
         return (
-            <p onClick={this.handleClick}>{this.props.name} hello welcome,You {text} this. Click toggle.</p>
-        );
+            <h1>Hello world, hello React!</h1>
+        )
     }
 });
 
+
+//two
+var Parent = React.createClass({
+    getDefaultProps : function(){ //设置 props 默认值
+        return {
+            context : 'default'
+        }
+    },
+    propTypes : {  //设置 props 的 类型和是否必须
+        context : React.PropTypes.string.isRequired
+    },
+    getInitialState : function(){
+        return {
+            context : this.props.context
+        }
+    },
+    render : function(){
+        console.log('Parent');
+        return (
+            <div>
+                <h1>This is Parent Title</h1>
+                <Child  content={this.state.context} />
+            </div>
+        )
+    }
+});
+
+var Child = React.createClass({
+    getInitialState : function(){
+        return {
+            content : this.props.content
+        }
+    },
+    render : function(){
+        console.log('Child');
+        return (
+            <div>
+                <h2>This is a Child Title</h2>
+                <p>{this.state.content}</p>
+                <button onClick={this.change}>Change</button>
+            </div>
+        )
+    },
+    change : function(){
+        this.setState({
+            content : 'OK'
+        })
+    }
+});
+
+
+//three
+var NotesList = React.createClass({
+    render : function(){
+        return (
+            <ol>
+                {
+                    React.Children.map(this.props.children,function(child,index){
+                        console.log(child,index);
+                        return <li>{child}</li>
+                    })
+                }
+            </ol>
+        )
+    }
+});
+
+//four
+
+var InputText = React.createClass({
+    getInitialState : function(){
+        return {
+            value : 'aa'
+        }
+    },
+    render : function(){
+        return (
+            <div>
+                <input type="text" ref='myInput' onChange={this.change}/>
+                <button onClick={this.focus} >focus</button>
+                <p>{this.state.value}</p>
+            </div>
+        )
+    },
+    change : function(ev){
+        this.setState({
+            value : this.refs.myInput.value
+        });
+        // or
+        // this.setState({
+        //     value : $(ev.target).val()
+        // });
+    },
+    focus : function(ev){
+        console.log(ev.target);
+        // 可以通过 event 对象 或 ref 来获取 DOM
+        $(ev.target).prev().val('aa')
+        $(this.refs.myInput).focus();
+    }
+})
+
+
 ReactDOM.render(
-    <LikeButton name="blue"/>,
-    document.getElementById('example')
-)
+    <InputText />
+,document.getElementById('test'));
